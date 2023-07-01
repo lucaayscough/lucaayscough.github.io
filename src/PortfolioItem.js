5007e74e0925e06d01070b0d7a20a5604e5fd0dd
lucaayscough <lucaayscough@gmail.com>
2023-07-01 12:17:28 +0100

    added item pages

--- src/PortfolioItem.js
+++ src/PortfolioItem.js
@@ -3,10 +3,11 @@ import portfolioData from "./portfolioData.json";
 
 function PortfolioItem() {
     let {id} = useParams();
+    const item = portfolioData.find((foundItem) => foundItem.id === Number(id));
 
     return(
         <div className="portfolioItem">
-            {id}
+            <h2>{item.title}</h2>
         </div>
     );
 };
