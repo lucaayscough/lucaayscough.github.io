985ff9db82d918ad8c0cf058b845d822d11f7715
lucaayscough <lucaayscough@gmail.com>
2023-07-23 18:24:14 +0100

    adding mdx support

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -4,71 +4,71 @@ import rehypePrettyCode from "rehype-pretty-code"
 import rehypeSlug from "rehype-slug"
 import remarkGfm from "remark-gfm"
 
-/** @type {import("contentlayer/source-files").computeFields} */
-const computeFields = {
-    slug: {
-        type: "string",
-        resolve: (doc) => `/${doc._raw.flattenedPath}`,
-    },
-    slugAsParams: {
-        type: "string",
-        resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
-    }
+/** @type {import("contentlayer/source-files").CmputeFields} */
+const computedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => `/${doc._raw.flattenedPath}`,
+  },
+  slugAsParams: {
+    type: "string",
+    resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+  }
 }
 
 export const Doc = defineDocumentType(() => ({
-    name: "Doc",
-    filePathPattern: "complete-nextjs/**/*.mdx",
-    contentType: "mdx",
-    fields: {
-        title: {
-            type: "string",
-            required: true,
-        },
-        description: {
-            type: "string",
-        },
-        published: {
-            type: "boolean",
-            default: true,
-        }
+  name: "Doc",
+  filePathPattern: "complete-nextjs/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
     },
-    computedFields,
+    description: {
+      type: "string",
+    },
+    published: {
+      type: "boolean",
+      default: true,
+    }
+  },
+  computedFields,
 }))
 
 export default makeSource({
-    contentDirPath: "src/content",
-    documentTypes: [Doc],
-    mdx: {
-        remarkPlugins: [remarkGfm],
-        rehypePlugins: [
-            rehypeSlug,
-            [
-                rehypePrettyCode,
-                {
-                    theme: "github-dark",
-                    onVisitLine(none) {
-                        if (node.children.length === 0) {
-                            node.children = [{ type: "text", value: " " }]
-                        }
-                    },
-                    onVisitHighlightedLine(node) {
-                        node.properties.className.push("line--highlighted")
-                    },
-                    onVisitHighlightedWord(node) {
-                        node.properties.className = ["word--highlighted"]
-                    }
-                },
-            ],
-            [
-                rehypeAutolinkHeadings,
-                {
-                    properties: {
-                        className: ["subheadings-anchor"],
-                        ariaLabel: "Link to section"
-                    },
-                },
-            ],
-        ]
-    },
+  contentDirPath: "src/content",
+  documentTypes: [Doc],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "github-dark",
+          onVisitLine(none) {
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }]
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted")
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"]
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["subheadings-anchor"],
+            ariaLabel: "Link to section"
+          },
+        },
+      ],
+    ]
+  },
 })
