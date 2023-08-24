e42c22f78fb6c5ed6889fde923ffc98587449b43
lucaayscough <lucaayscough@gmail.com>
2023-08-24 14:45:08 +0100

    changed styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,14 +5,19 @@ function Header() {
   return (
     <header className="flex flex-col items-center justify-center p-12">
       <div className="nameHeader">LUCA AYSCOUGH</div>
-
-      <nav className="flex flex-row">
-        <Link className="headerLink" href="/">HOME</Link>
-        <Link className="headerLink" href="/portfolio">PORTFOLIO</Link>
-        <Link className="headerLink" href="mailto:lucaayscough@gmail.com">CONTACT</Link>
-      </nav>
-
       
+
+      <label className="hamburgerMenu">
+        <input type="checkbox"></input>
+      </label>
+
+      <aside className="navBar">
+        <nav>
+          <Link className="headerLink" href="/">HOME</Link>
+          <Link className="headerLink" href="/portfolio">PORTFOLIO</Link>
+          <Link className="headerLink" href="mailto:lucaayscough@gmail.com">CONTACT</Link>
+        </nav>
+      </aside>
     </header>
   )
 }
