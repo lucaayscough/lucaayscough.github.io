7f10efa74bfb885d0e0e14a62b9fcb44cdca7156
lucaayscough <lucaayscough@gmail.com>
2023-08-24 12:09:25 +0100

    changed styles and fixed content

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,14 +3,16 @@ import "./header.css"
 
 function Header() {
   return (
-    <header>
+    <header className="flex flex-col items-center justify-center p-12">
       <div className="nameHeader">LUCA AYSCOUGH</div>
 
-      <nav className="headerLinks">
+      <nav className="flex flex-row">
         <Link className="headerLink" href="/">HOME</Link>
         <Link className="headerLink" href="/portfolio">PORTFOLIO</Link>
         <Link className="headerLink" href="mailto:lucaayscough@gmail.com">CONTACT</Link>
       </nav>
+
+      
     </header>
   )
 }
