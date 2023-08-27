a2d285f30dfbbaa78a2d52c4b685c299ed434e7f
lucaayscough <lucaayscough@gmail.com>
2023-08-27 15:55:50 +0100

    added content integration

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,8 +1,28 @@
+import Image from "next/image"
+import { compareDesc, format, parseISO } from 'date-fns'
+import { allPortfolios } from 'contentlayer/generated'
+import "./page.css"
+
 export default function Media() {
+  const posts = allPortfolios.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+
   return (
     <main>
       <h1>media</h1>
 
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-12">
+        {posts.map((item, idx) => (
+          <div key="idx" className="">
+            <Image
+              priority
+              src={`/images/media/${item.title.replace(/ /g, '_').toLowerCase()}.png`}
+              height={1}
+              width={384}
+              alt={item.title}
+            />
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
