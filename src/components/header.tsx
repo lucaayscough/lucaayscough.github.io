69c0ec50de87f0c179821da0954bcb81e5e5c4a5
lucaayscough <lucaayscough@gmail.com>
2023-08-27 14:54:20 +0100

    added fonts and changed header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link"
+import Image from "next/image"
 import "./header.css"
 
 function Header() {
   return (
     <header className="flex flex-col items-center justify-center p-12 gap-8">
-      <div className="nameHeader">LUCA AYSCOUGH</div>
+      <div>
+        <Image
+          priority
+          src="/images/header.svg"
+          height={1}
+          width={440}
+          alt="Luca Ayscough"
+        />
+      </div>
 
       <div className="redBar"></div>
       
@@ -12,7 +21,7 @@ function Header() {
         <Link className="headerLink" href="/">ABOUT</Link>
         <Link className="headerLink" href="/media">MEDIA</Link>
         <Link className="headerLink" href="/music">MUSIC</Link>
-        <Link className="headerLink" href="/software">SOFTWARE</Link>
+        <Link className="headerLink" target="_blank" href="https://chromaaudio.gumroad.com">SOFTWARE</Link>
         <Link className="headerLink" href="mailto:lucaayscough@gmail.com">CONTACT</Link>
       </nav>
     </header>
