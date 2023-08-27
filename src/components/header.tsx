7a90b9eb6579752ae59171e5d4e9dfe98be4ca30
lucaayscough <lucaayscough@gmail.com>
2023-08-27 14:16:13 +0100

    redesign

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,16 @@ import "./header.css"
 
 function Header() {
   return (
-    <header className="flex flex-col items-center justify-center p-12">
+    <header className="flex flex-col items-center justify-center p-12 gap-8">
       <div className="nameHeader">LUCA AYSCOUGH</div>
+
+      <div className="redBar"></div>
       
       <nav className="navBar">
-        <Link className="headerLink" href="/">HOME</Link>
-        <Link className="headerLink" href="/portfolio">PORTFOLIO</Link>
+        <Link className="headerLink" href="/">ABOUT</Link>
+        <Link className="headerLink" href="/media">MEDIA</Link>
+        <Link className="headerLink" href="/music">MUSIC</Link>
+        <Link className="headerLink" href="/software">SOFTWARE</Link>
         <Link className="headerLink" href="mailto:lucaayscough@gmail.com">CONTACT</Link>
       </nav>
     </header>
