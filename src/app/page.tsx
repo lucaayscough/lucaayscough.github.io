418c6c87c125fc3c39e7f9480ee4876268fde1b2
lucaayscough <lucaayscough@gmail.com>
2023-08-30 12:45:22 +0100

    changed about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       
 
       <p>
-        I am also the founder of the Chroma Project, a free and open-source software company, under which I develop audio plug-ins. One such project employs a generative adversarial neural network, which outputs audio samples through the exploration of a latent space based on a large dataset of raw audio. I have also built a pitch tracker which based on monophonic audio input, creates a histogram visualisation of the pitch contour and can convert the estimated frequency into MPE midi data. These projects have given me the confidence to learn highly technical subjects in a self-supervised manner and to apply the knowledge in creative ways.
+        I am also the founder of the Chroma Project, a free and open-source software company, under which I develop audio plug-ins. One such project employs a generative adversarial neural network, which outputs audio samples through the exploration of a latent space based on a large dataset of raw audio. I have also built a pitch tracker which based on monophonic audio input, creates a histogram visualisation of the pitch contour and can convert the estimated frequency into MPE midi data.
       </p>
 
       <Image
