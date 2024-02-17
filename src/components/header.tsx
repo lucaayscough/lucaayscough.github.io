1eaa62ba41029898e811412672239c75b4b1a47e
lucaayscough <lucaayscough@gmail.com>
2024-02-17 12:50:00 +0000

    changed link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,7 +20,7 @@ function Header() {
         <Link className="headerLink" href="/">ABOUT</Link>
         <Link className="headerLink" href="/media">MEDIA</Link>
         {/* <Link className="headerLink" href="/music">MUSIC</Link> */}
-        <Link className="headerLink" target="_blank" href="https://chromaaudio.gumroad.com">SOFTWARE</Link>
+        <Link className="headerLink" target="_blank" href="https://lucaayscough.gumroad.com">SOFTWARE</Link>
         <Link className="headerLink" href="mailto:lucaayscough@gmail.com">CONTACT</Link>
       </nav>
     </header>
